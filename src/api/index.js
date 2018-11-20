c97bfb1fcf12b20b6de51f4c49c34b65937d7beb
bettoarcaya <betoarcaya@gmail.com>
2018-11-20 12:50:41 -0400

    creando metodo para conectar con la api y obtener los datos necesarios

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import config from './config'
+
+
+const API_KEY = config.apiKey;
+const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=venezuela&api_key=${API_KEY}&format=json`;
+
+
+function getArtists(){
+    fetch(URL)
+        .then( response => response.json )
+        .then( json => json.topartist.artists );
+}
